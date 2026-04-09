5483f835f3cf4e873261c905f9152e7cfdb9ccd2
sandun18 <sandunanjana333@gmail.com>
2026-04-09 12:14:46 +0530

    add test2.js and test3.js with file reading and greeting functionality

---
+++ test3.js
@@ -0,0 +1,12 @@
+let name = 'node.js';
+const version = 20;
+
+function greet(user){
+    return `Hello, ${user}!`
+}
+
+const add =(a,b) =>a + b;
+
+console.log(greet('Developer'));
+console.log(add(5,3));
+
